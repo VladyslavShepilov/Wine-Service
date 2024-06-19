cfa144b2f18178a62c1a422264aa141a1d4f73d8
Vladyslav Shepilov <shepsterya@gmail.com>
2024-06-19 11:22:53 +0300

    add wine model

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Catalog(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    grape_variety = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    alcohol_content = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    price = models.PositiveIntegerField()
+    amount = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-price"]
