48479ce969bcef26a5032b6882f35fcff6d6e659
Vladyslav Shepilov <shepsterya@gmail.com>
2024-06-19 10:02:06 +0300

    add serializers for new user model

--- backend/wine_service/settings.py
+++ backend/wine_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -99,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = "user.User"
 
 
 # Internationalization
