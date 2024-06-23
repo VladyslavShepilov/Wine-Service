3eacd4fffc7e0928dc70d7064ce96585d5dfcd95
Vladyslav Shepilov <shepsterya@gmail.com>
2024-06-23 11:56:32 +0300

    add serializer, which tracks the amount of ordered positions with proper validation, add views and serializers to display them

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -13,13 +13,24 @@ class PositionSerializer(serializers.ModelSerializer):
             attrs["quantity"], data["wine_position"].amount,
             ValidationError
         )
+        return attrs
 
     class Meta:
         model = Position
         fields = [
             "quantity",
             "wine_position",
-            "order",
+        ]
+
+
+class PositionDetailSerializer(serializers.ModelSerializer):
+    wine_position = serializers.SlugRelatedField(slug_field="name", read_only=True)
+
+    class Meta:
+        model = Position
+        fields = [
+            "wine_position",
+            "quantity",
         ]
 
 
@@ -42,11 +53,35 @@ class OrderSerializer(serializers.ModelSerializer):
             for position_data in positions_data:
                 Position.objects.create(order=order, **position_data)
 
+                wine_position = position_data["wine_position"]
+                quantity = position_data["quantity"]
+
+                wine_position.amount -= quantity
+                wine_position.save()
+
+        return order
+
 
 class OrderListSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(slug_field="email", read_only=True, many=False)
+
     class Meta:
         model = Order
         fields = [
+            "id",
             "created_at",
             "user",
+        ]
+
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    positions = PositionDetailSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = [
+            "id",
+            "positions",
+            "user",
+            "created_at",
         ]
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,16 +1,35 @@
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAdminUser
 
 from orders.models import Order
-from orders.serializers import OrderSerializer, OrderListSerializer
+from orders.serializers import OrderSerializer, OrderListSerializer, OrderDetailSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.prefetch_related("positions")
+    queryset = Order.objects.prefetch_related("positions", "user")
     serializer_class = OrderSerializer
-    permission_classes = (AllowAny,)
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action == "list":
+            queryset = queryset.prefetch_related("user")
+        elif self.action == "retrieve":
+            queryset = queryset.prefetch_related("positions__wine_position__amount", "user")
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
             return OrderListSerializer
+
+        if self.action == "retrieve":
+            return OrderDetailSerializer
+
         return OrderSerializer
+
+    def perform_create(self, serializer):
+        if not self.request.user.is_staff:
+            serializer.save(user=self.request.user)
+        else:
+            serializer.save()
